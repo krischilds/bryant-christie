528805311e44fc74b5509150a153d1055ab557a4
Kris Childs <kris.childs@mvtransit.com>
2020-07-07 23:51:29 -0700

    Updates

---
+++ precache-manifest.6e2f3218f6f6d1ed42aa67442bb0ffd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcc6ead9f7188f9fad7208bf7b4a056",
+    "url": "/bryant-christie/index.html"
+  },
+  {
+    "revision": "e7ba69b305b9e04723d1",
+    "url": "/bryant-christie/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b57eb337f68daf19b0fb",
+    "url": "/bryant-christie/static/css/main.6371fc9f.chunk.css"
+  },
+  {
+    "revision": "e7ba69b305b9e04723d1",
+    "url": "/bryant-christie/static/js/2.a21dcdf2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bryant-christie/static/js/2.a21dcdf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57eb337f68daf19b0fb",
+    "url": "/bryant-christie/static/js/main.d1d62133.chunk.js"
+  },
+  {
+    "revision": "38c70b7d8918d9b9305b",
+    "url": "/bryant-christie/static/js/runtime-main.337506eb.js"
+  }
+]);
